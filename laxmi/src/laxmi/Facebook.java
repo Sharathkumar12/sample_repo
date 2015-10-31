96322e0ad24ff105fad0dc252acd3f3603033bee
admin <admin>
2015-10-31 15:06:16 +0530

    committed

---
+++ laxmi/src/laxmi/Facebook.java
@@ -0,0 +1,15 @@
+package laxmi;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class Facebook {
+	FirefoxDriver driver;
+	@BeforeMethod
+	public void setup()
+	{
+		driver=new FirefoxDriver();
+		driver
+	}
+
+}
